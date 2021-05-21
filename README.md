44fe33be80aea6320afb9c448b14b150093270d2
Matei-Alexandru Gardus <matei@gard.us>
2021-05-21 02:26:30 -0700

    Write all unit tests basis
    Co-authored-by: Aksharan Saravanan <aksharans@users.noreply.github.com>

--- README.md
+++ README.md
@@ -5,12 +5,24 @@
     - (1) Within a Github action that runs whenever code is pushed 
 
 2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
-    - No, because the message feature likely requires individual components interacting on an application & feature level. For this feature, there are 2 users who each have some sort of messaging component on their end, and when a message is sent, other parts, or the application as a whole should handle storing and transmitting the data from one user to the next, in addition to all the features that have to be implemented to ensure that 2 users can send data to each other. 
+    - No, because the message feature likely requires individual components interacting on an application & feature level. For this feature, there are 2 users who each have some sort of messaging component on their end, and when a message is sent, other parts, or the application as a whole should handle storing and transmitting the data from one user to the next, in addition to all the features that have to be implemented to ensure that 2 users can send data to each other. This test is better suited for an integration test, rather than a unit test.
 
 3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
-    - Yes, because the max message length feature is an individual restriction, or implementation, within the  messaging feature for a user, so it is plausible to test that this inidividual feature is working as it should. The max length feature is probably some check within the input field, so it should be possible to unit test that particular field.
+    - Yes, because the max message length feature is an individual restriction, or implementation, within the messaging feature for a user, so it is plausible to test that this inidividual feature is working as it should. The max length feature is probably some check within the input field, so it should be possible to unit test that particular field.
 
 4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
+    - If we run our Puppeteer tests with the "headless" field set to true, we expect for our tests to run using a headless version of the Chromium browser; in other words, the browser spawns without a graphical interface, but can be used internally 
 
 5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
 
+    - The `beforeAll` callback would require a goto towards the settings page, likely using the `click` functionality rather than a direct URL usage. Something like this works:
+
+    ```js
+    beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5500);
+    const settingIcon = await page.$('header > img');
+
+    await settingIcon.click();
+    await page.waitForNavigation();
+    });
+    ```
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -13,95 +13,253 @@ describe('Basic user flow for SPA ', () => {
   });
 
   // test 2 is given
-  it('Test2: Make sure <journal-entry> elements are populated', async () => {
-    let allArePopulated = true;
-    let data, plainValue;
-    const entries = await page.$$('journal-entry');
-    for (let i = 0; i < entries.length; i++) {
-      data = await entries[i].getProperty('entry');
-      plainValue = await data.jsonValue();
-      if (plainValue.title.length == 0) { allArePopulated = false; }
-      if (plainValue.date.length == 0) { allArePopulated = false; }
-      if (plainValue.content.length == 0) { allArePopulated = false; }
-    }
-    expect(allArePopulated).toBe(true);
-  }, 30000);
+  // it('Test2: Make sure <journal-entry> elements are populated', async () => {
+  //   let allArePopulated = true;
+  //   let data, plainValue;
+  //   const entries = await page.$$('journal-entry');
+  //   for (let i = 0; i < entries.length; i++) {
+  //     data = await entries[i].getProperty('entry');
+  //     plainValue = await data.jsonValue();
+  //     if (plainValue.title.length == 0) { allArePopulated = false; }
+  //     if (plainValue.date.length == 0) { allArePopulated = false; }
+  //     if (plainValue.content.length == 0) { allArePopulated = false; }
+  //   }
+  //   expect(allArePopulated).toBe(true);
+  // }, 30000);
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
-    // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
+    const entries = await page.$$('journal-entry');
+    const firstEntry = entries[0];
+    
+    await firstEntry.click();
+    await page.waitForNavigation();
 
-  });
+    const urlOfPage = new URL(page.url());
+    expect(urlOfPage.hash).toBe('#entry1');
+  }, 10000);
 
   it('Test4: On first Entry page - checking page header title', async () => {
-    // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe('Entry 1');
   });
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
-    /*
-     implement test5: Clicking on the first journal entry should contain the following contents: 
-        { 
-          title: 'You like jazz?',
-          date: '4/25/2021',
-          content: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
-          image: {
-            src: 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455',
-            alt: 'bee with sunglasses'
-          }
-        }
-      */
+    const journalContents = { 
+      title: 'You like jazz?',
+      date: '4/25/2021',
+      content: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
+      image: {
+        src: 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455',
+        alt: 'bee with sunglasses'
+      }
+    };
 
+    const entryTitle = await page.$eval('pierce/.entry-title', (element) => {
+      return element.innerHTML
+    });
+    const entryDate = await page.$eval('pierce/.entry-date', (element) => {
+      return element.innerHTML
+    });
+    const entryContent = await page.$eval('pierce/.entry-content', (element) => {
+      return element.innerHTML
+    });
+    const entryImageSrc = await page.$eval('pierce/.entry-image', (element) => {
+      return element.getAttribute('src');
+    });
+    const entryImageAlt = await page.$eval('pierce/.entry-image', (element) => {
+      return element.getAttribute('alt');
+    });
+    
+    expect(entryTitle).toBe(journalContents.title);
+    expect(entryDate).toBe(journalContents.date);
+    expect(entryContent).toBe(journalContents.content);
+    expect(entryImageSrc).toBe(journalContents.image.src);
+    expect(entryImageAlt).toBe(journalContents.image.alt);
   }, 10000);
 
   it('Test6: On first Entry page - checking <body> element classes', async () => {
-    // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
+    const body = await page.$('body');
+    const bodyClass = await body.getProperty('className');
+    const bodyClassArray = await bodyClass.jsonValue();
 
+    expect(bodyClassArray).toBe('single-entry');
   });
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
-    // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
+    const settingIcon = await page.$('header > img');
 
-  });
+    await settingIcon.click();
+    await page.waitForNavigation();
+
+    const urlOfPage = new URL(page.url());
+    expect(urlOfPage.hash).toBe('#settings');
+  }, 10000);
 
   it('Test8: On Settings page - checking page header title', async () => {
-    // implement test8: Clicking on the settings icon should update the header to be “Settings”
-
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe('Settings');
   });
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
-    // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
+    const body = await page.$('body');
+    const bodyClass = await body.getProperty('className');
+    const bodyClassArray = await bodyClass.jsonValue();
 
+    expect(bodyClassArray).toBe('settings');
   });
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
-    // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
+    await page.goBack();
 
+    const urlOfPage = new URL(page.url());
+    expect(urlOfPage.hash).toBe('#entry1');
+  }, 10000);
+
+  it('Test11: Clicking the back button once should bring the user back to the home page', async () => {
+    await page.goBack();
+
+    const urlOfPage = new URL(page.url());
+    expect(urlOfPage.pathname).toBe('/');
+  }, 10000);
+
+
+  it('Test12: When the user if on the homepage, the header title should be “Journal Entries”', async () => {
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe('Journal Entries');
   });
 
-  // define and implement test11: Clicking the back button once should bring the user back to the home page
+
+  it('Test13: On the home page the <body> element should not have any class attribute ', async () => {
+    const body = await page.$('body');
+    const bodyClass = await body.getProperty('className');
+    const bodyClassArray = await bodyClass.jsonValue();
+    expect(bodyClassArray).toBe("");
+  });
 
 
-  // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
+  it('Test14: Verify the url is correct when clicking on the second entry', async () => {
+    const entries = await page.$$('journal-entry');
+    const secondEntry = entries[1];
+    
+    await secondEntry.click();
+    await page.waitForNavigation();
+
+    const urlOfPage = new URL(page.url());
+    expect(urlOfPage.hash).toBe('#entry2');
+  }, 10000);
 
 
-  // define and implement test13: On the home page the <body> element should not have any class attribute 
+  it('Test15: Verify the title is current when clicking on the second entry', async () => {
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe('Entry 2');
+  });
 
 
-  // define and implement test14: Verify the url is correct when clicking on the second entry
+  it('Test16: Verify the entry page contents is correct when clicking on the second entry', async () => {
+    const journalContents = {
+      date: "4/26/2021",
+      title: "Run, Forrest! Run!",
+      content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+      image: {
+          src: "https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg",
+          alt: "forrest running"
+      },
+      audio: null
+    };
 
+    const entryTitle = await page.$eval('pierce/.entry-title', (element) => {
+      return element.innerHTML
+    });
+    const entryDate = await page.$eval('pierce/.entry-date', (element) => {
+      return element.innerHTML
+    });
+    const entryContent = await page.$eval('pierce/.entry-content', (element) => {
+      return element.innerHTML
+    });
+    const entryImageSrc = await page.$eval('pierce/.entry-image', (element) => {
+      return element.getAttribute('src');
+    });
+    const entryImageAlt = await page.$eval('pierce/.entry-image', (element) => {
+      return element.getAttribute('alt');
+    });
+    
+    expect(entryTitle).toBe(journalContents.title);
+    expect(entryDate).toBe(journalContents.date);
+    expect(entryContent).toBe(journalContents.content);
+    expect(entryImageSrc).toBe(journalContents.image.src);
+    expect(entryImageAlt).toBe(journalContents.image.alt);
+  }, 10000);
 
-  // define and implement test15: Verify the title is current when clicking on the second entry
+  it('Test17: "Settings" navigation bar has different color', async () => {
+    await page.goBack();
 
+    const settingIcon = await page.$('header > img');
 
-  // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+    await settingIcon.click();
+    await page.waitForNavigation();
 
+    const navbarColor = await page.$eval("header", (element) => {
+      return window.getComputedStyle(element).getPropertyValue('background-color');
+    });
+    expect(navbarColor.backgroundColor).toBe('#9d9d9d');
+  
+  }, 20000);
 
-  // create your own test 17
+  it('Test18: Navbar color changes back when reaching main page', async () => {
+    await page.goBack();
 
-  // create your own test 18
+    const navbarColor = await page.$eval("header", (element) => {
+      return window.getComputedStyle(element).getPropertyValue('background-color');
+    });
+    expect(navbarColor).toBe('#D9D281');
+  }, 10000);
 
-  // create your own test 19
+  it('Test19: No extraneous audio placed on single entry pages with no audio content', async () => {
+    const entries = await page.$$('journal-entry');
+    const firstEntry = entries[0];
+    
+    await firstEntry.click();
+    await page.waitForNavigation();
 
-  // create your own test 20
+    const journalContents = { 
+      title: 'You like jazz?',
+      date: '4/25/2021',
+      content: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
+      image: {
+        src: 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455',
+        alt: 'bee with sunglasses'
+      }
+    };
+
+    const entryAudio = await page.$('.entry-audio');
+    expect(entryAudio).toBe(null);
+  }, 10000);
+
+  it('Test20: Audio placed on single entry pages with audio content', async () => {
+    const journalContents = {
+      date: "4/28/2021",
+      title: "You're a wizard, Harry",
+      content: "Hmm, difficult. VERY difficult. Plenty of courage, I see. Not a bad mind, either. There's talent, oh yes. And a thirst to prove yourself. But where to put you? Not Slytherin. Not Slytherin. Not Slytherin, eh? Are you sure? You could be great, you know. It's all here in your head. And Slytherin will help you on the way to greatness, there's no doubt about that. No? Please, please. Anything but Slytherin, anything but Slytherin. Well if you're sure, better be... GRYFFINDOR!",
+      image: {
+          src: "https://w7w5t4b3.rocketcdn.me/wp-content/uploads/2019/01/harry-potter-sorting-hat-wrong.jpg",
+          alt: "harry looking up at the sorting hat"
+      },
+      audio: "https://drive.google.com/uc?export=download&id=1Orwnly-OMhNt83tb-SAWt6Y3S6AYQgkk"
+    };
+
+    await page.goBack();
+
+    const entries = await page.$$('journal-entry');
+    const thirdEntry = entries[3];
+    
+    await thirdEntry.click();
+    await page.waitForNavigation();
+
+    const entryAudioSrc = await page.$eval('pierce/.entry-audio', (element) => {
+      return element.getAttribute('src');
+    });
+    
+    expect(entryAudioSrc).toBe(journalContents.audio);
+  }, 20000);
   
 });
